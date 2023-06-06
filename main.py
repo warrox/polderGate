9328421ca6c554dfa421f3c6f88679bf26c98830
warrox <100@guybuydie.com>
2023-06-06 20:50:22 +0200

    (Kivy basic script)

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world first")
+import kivy
+kivy.require('2.2.0') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
