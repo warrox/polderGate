00fefbe3b10278b58556fbfb287d85effb1f324a
warrox <100@guybuydie.com>
2023-06-09 11:16:31 +0200

    compress_images.py(adding pillow_heif to convert .heif to jpeg)

--- compress_images.py
+++ compress_images.py
@@ -1,17 +1,19 @@
 from PIL import Image, ImageOps
+from pillow_heif import register_heif_opener
 import os.path
 
-def downsize_images(pic):
+def downsize_images(file_path):
     l = 0
-    for picture in range(0,len(pic)):
-        t = Image.open((pic[picture]))  # My image is a 200x374 jpeg that is 102kb large
+    for picture in range(0, len(file_path)):
+        register_heif_opener()
+        t = Image.open((file_path[picture]))  # My image is a 200x374 jpeg that is 102kb large
         fixed_image = ImageOps.exif_transpose(t)
         # downsize the image with an ANTIALIAS filter (gives the highest quality)
         fixed_image.thumbnail((300,300),Image.LANCZOS)
         fixed_image.save(f'/Users/warren/PolderGate/image_scaled{str(l)}.jpeg', quality=95)
         l += 1
         #picture += 1
-        if picture == len(pic):
+        if picture == len(file_path):
             break
 
     #foo.save('/Users/warren/Downloads/image_scaled.jpeg', quality=95)  # The saved downsized image size is 24.8kb
--- main.py
+++ main.py
@@ -6,19 +6,19 @@ import os
 import add_photos
 import compress_images
 
+
 def start_program():
     p = add_photos.add_path_pictures()
     compress_images.downsize_images(p)
-parent_dir = "/Users/warren/"
+
+
+parent_dir = os.path.expanduser('~')
 directory = "PolderGate"
 path = os.path.join(parent_dir, directory)
 
 if os.path.exists(path):
-   start_program()
+    start_program()
 else:
     os.mkdir(path)
     print("Directory '% s' created" % directory)
     start_program()
-
-
-
